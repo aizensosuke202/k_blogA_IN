f2531c5e80d0ec914dc4d0d32e051392ea6c1c8e
MasterKey120 <alonedoomsday@gmail.com>
2025-08-22 02:01:09 +0000

    correction et ajout de laa verification du mot de passe lors de la creation du compte

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const pool = require('../db');
 const authenticateToken = require('../middleware/authMiddleware');
-const { body, validationResult} = rquire('express-validator');
+const { body, validationResult} = require('express-validator');
 
 router.post('/', [
     body('username').isLength({min: 3}),
@@ -22,6 +22,11 @@ router.post('/', [
 router.post('/', async (req, res) => {
     try {
         const { username, email, passwd } = req.body;
+        const userExists = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
+
+        if(userExists.rows.length > 0){
+            return res.status(400).json({error:'Email deja utilisee!'});
+        }
 
         const hashedPasswd = await bcrypt.hash(passwd, saltRounds);
         const result = await pool.query(
@@ -78,6 +83,20 @@ router.put('/:id', async (req, res) => {
         console.error(err.message);
         res.status(500).send('Erreur serveur');
     }
+});
+router.put('/:id', async (req,res)=>{
+    try{
+        const { id } = req.params;
+        const { passwd } = req.body;
+        const result = await pool.query('Update users set passwd = $1', [passwd]);
+          if(result.rows.lenght === 0){
+        return res.status(404).json({message: 'le mot de passe non existant'});
+        } 
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur serveur');
 });
 
 // DELETE user
